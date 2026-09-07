ff223883dcbad9ea87599fd696b6cbbee2cca9c4
databong001-maker <databong001@gmail.com>
2026-09-07 12:00:34 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE='jeju-planner-shell-v3';
-const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./stops.js'];
 
 self.addEventListener('install',(e)=>{
   e.waitUntil(caches.open(CACHE).then((c)=>c.addAll(ASSETS)));
